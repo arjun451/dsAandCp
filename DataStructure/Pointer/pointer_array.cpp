ac615c768bf230b5bf35477beb8045a3552a47f6
arjun451 <aksingh25071998@gmail.com>
2022-12-19 19:42:08 +0530

    pointer array compresion

---
+++ DataStructure/Pointer/pointer_array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10];
+    cout<<a<<endl;
+    cout<<&a[0]<<endl;
+    a[0]=5;
+    a[1]=10;
+    cout<<*a<<endl;
+    cout<<*(a+1)<<endl;
+    //cout<<1[a]<<endl;
+    int *p = &a[0];
+    cout<<p<<endl;
+    cout<<a<<endl;
+    cout<<&p<<endl;
+    cout<<&a<<endl;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(p)<<endl;
+    p=p+1;
+   // a=a+1;      
+    return 0;
+}
