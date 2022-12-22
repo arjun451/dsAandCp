f4c0f46837a9273795e2f76137173922ac2b0922
arjun451 <aksingh25071998@gmail.com>
2022-12-22 21:40:52 +0530

    first index array

---
+++ DataStructure/Recursion/first_index.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int firstIndex(int ar[],int size,int x)
+{
+
+	if(size==-1)
+	{
+		return -1;
+	}
+	int result = firstIndex(ar,size-1,x);
+	if(result==-1)
+	{
+		if(ar[size]==x)
+			return size;
+		else
+			return -1;
+	
+	}
+	else
+		return result;
+}
+
+
+int main()
+{
+
+	int n;
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	int *ar = new int[n];
+	cout<<"Enter the element of the array:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>ar[i];
+	}
+
+	cout<<"Enter the number to find the index: "<<endl;
+	int x;
+	cin>>x;
+	cout<<firstIndex(ar,n-1,x);
+}
