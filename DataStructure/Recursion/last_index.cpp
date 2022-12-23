c6ef28e9c1cf9e40e58290f69663cea9832ff678
arjun451 <aksingh25071998@gmail.com>
2022-12-23 21:46:44 +0530

    find last index second way

--- DataStructure/Recursion/last_index.cpp
+++ DataStructure/Recursion/last_index.cpp
@@ -9,6 +9,24 @@ int lastIndex(int ar[],int size,int x)
 	int result = lastIndex(ar,size-1,x);
 	return result;
 }
+
+int lastIndex2(int ar[],int size,int x)
+{
+        if(size==-1)
+                return -1;
+       
+        int result = lastIndex(ar+1,size-1,x);
+	if(result==-1)
+	{
+
+		if(ar[0]==x)
+		  result =0;
+	}
+	else
+		result++;
+
+        return result;
+}
 int main()
 {
 	int n;
@@ -23,7 +41,8 @@ int main()
         cout<<"Enter the element to find index: ";
 	int x;
 	cin>>x;
-	cout<<lastIndex(ar,n-1,x);
+	cout<<lastIndex(ar,n-1,x)<<endl;
+	cout<<lastIndex2(ar,n-1,x)<<endl;
 
-
+         
 }
