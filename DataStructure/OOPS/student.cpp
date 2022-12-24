5b9658e0f7d14e12714c577e450a9cd8413326fd
arjun451 <aksingh25071998@gmail.com>
2022-12-25 01:03:58 +0530

    constructor introduced

--- DataStructure/OOPS/student.cpp
+++ DataStructure/OOPS/student.cpp
@@ -5,6 +5,25 @@ class Student{
                 int age ;
 
 	public :
+		//default constructor
+		Student()
+		{
+			cout<<"Default constructor called!"<<endl;
+		}
+       
+		//parameterised constructor
+		Student(int age)
+		{
+			cout<<"Parameterised constructor called!"<<endl;
+			cout<<"this "<<this<<endl;
+		      ( *this).age = age;
+		}
+		 Student(int a,int r)
+                {
+                        cout<<"Parameterised constructor 2 called!"<<endl;
+                        age = a;
+			rollnum =r;
+                }
                 void display()
 		{
 			cout<<"(rollnumber,age): "<<rollnum<<","<<age<<endl;
--- DataStructure/OOPS/studentUse.cpp
+++ DataStructure/OOPS/studentUse.cpp
@@ -3,12 +3,15 @@ using namespace std;
 #include "student.cpp"
 int main()
 {
-	Student s1;
-	s1.setAge(24);
+	Student s1(20);
+	cout<<"address of s1: "<<&s1<<endl;
+//	s1.setAge(24);
 	s1.rollnum = 1;
 	cout<<"s1.age=>"<<s1.getAge()<<", s1.rollnum=> " <<s1.rollnum<<endl;
       //  s1.display();
-	Student s2,s3;
+	Student s2(3,22),s3(4,25);
+	s2.display();
+	s3.display();
 	Student *s4 = new Student;
 	(*s4).setAge(22); 
 	s4->rollnum=2;
