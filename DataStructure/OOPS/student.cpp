b3b0bd7d1f743194fe8f0d4e25aa6205909e42e2
arjun451 <aksingh25071998@gmail.com>
2022-12-24 23:42:41 +0530

    getters and setters in obj

--- DataStructure/OOPS/student.cpp
+++ DataStructure/OOPS/student.cpp
@@ -1,7 +1,22 @@
 class Student{
 	public :
-       int rollnum;
-       int age ;
+                int rollnum;
+	private :
+                int age ;
+
+	public :
+                void display()
+		{
+			cout<<"(rollnumber,age): "<<rollnum<<","<<age<<endl;
+		}
+		int getAge()
+		{
+			return age;
+		}
+		void setAge(int a)
+		{
+			age = a;
+		}
 
 
 
--- DataStructure/OOPS/studentUse.cpp
+++ DataStructure/OOPS/studentUse.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main()
 {
 	Student s1;
-	s1.age = 24;
+	s1.setAge(24);
 	s1.rollnum = 1;
-	cout<<"s1.age=>"<<s1.age<<" s1.rollnum=> " <<s1.rollnum<<endl;
+	cout<<"s1.age=>"<<s1.getAge()<<", s1.rollnum=> " <<s1.rollnum<<endl;
+      //  s1.display();
 	Student s2,s3;
 	Student *s4 = new Student;
-	(*s4).age = 20;
-	s4->rollnum=54;
-	 cout<<"s4.age=>"<<(*s4).age<<" s4.rollnum=> " <<s4->rollnum<<endl;
-
+	(*s4).setAge(22); 
+	s4->rollnum=2;
+	 cout<<"s4.age=>"<<(*s4).getAge()<<" s4.rollnum=> " <<s4->rollnum<<endl;
+      //   s4->display();
 }
 
